9398cb9eb7f70cc512a08787d7e64049b0dcad99
Your Name <you@example.com>
2016-09-10 04:21:58 +0200

    install

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 sudo add-apt-repository -y ppa:ethereum/ethereum
 sudo add-apt-repository -y ppa:ethereum/ethereum-dev
 sudo add-apt-repository -y ppa:webupd8team/atom
+sudo apt-get update
+sudo apt-get install -y build-essential libgmp-dev curl git atom software-properties-common ethereum
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-sudo apt-get install nodejs curl git atom software-properties-common ethereum
+sudo apt-get install -y nodejs
 sudo apt-get upgrade -y
 sudo apt-get autoremove --purge -y
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.7/install.sh | bash
 source ~/.bashrc
 nvm install 5
-sudo npm install -g grunt-cli embark-framework ethersim ethereumjs-testrpc
-testrpc --port 8101
-embark run
+sudo npm install -g grunt-cli embark-framework ethersim
