0b743a01668b438e311b426a2cd94880cd082f39
Your Name <you@example.com>
2016-09-10 03:37:17 +0200

    install

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo add-apt-repository -y ppa:ethereum/ethereum
+sudo add-apt-repository -y ppa:ethereum/ethereum-dev
+sudo add-apt-repository -y ppa:webupd8team/atom
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+sudo apt-get install nodejs curl git atom software-properties-common ethereum
+sudo apt-get upgrade -y
+sudo apt-get autoremove --purge -y
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.7/install.sh | bash
+source ~/.bashrc
+nvm install 5
+sudo npm install -g grunt-cli embark-framework ethereumjs-testrpc
+testrpc --port 8101
+embark run
